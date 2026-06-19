fc4f71d4389450da5ff037f78e7166027aef97dc
ArseniAstonMartin <zarsenij2506@gmail.com>
2026-06-19 17:03:38 +0300

    chore: initialize project baseline

---
+++ notification/src/main/kotlin/com/krainet/notification/NotificationApplication.kt
@@ -0,0 +1,11 @@
+package com.krainet.notification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotificationApplication>(*args)
+}
