c3e343c8d74200041e6a0a1675ad8930d32076c6
ArseniAstonMartin <zarsenij2506@gmail.com>
2026-07-03 19:03:11 +0300

    -

--- notification/src/main/kotlin/com/krainet/notification/NotificationApplication.kt
+++ notification/src/main/kotlin/com/krainet/notification/NotificationApplication.kt
@@ -2,12 +2,10 @@ package com.krainet.notification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableCaching
 class NotificationApplication
 
 fun main(args: Array<String>) {
--- notification/src/main/kotlin/com/krainet/notification/service/AdminEmailLookupService.kt
+++ notification/src/main/kotlin/com/krainet/notification/service/AdminEmailLookupService.kt
@@ -1,19 +1,17 @@
 package com.krainet.notification.service
 
-import com.krainet.notification.client.AuthAdminClient
+import com.krainet.notification.client.AdminEmailsClient
 import org.slf4j.LoggerFactory
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
 class AdminEmailLookupService(
-    private val adminEmailClient: AdminEmailClient,
+    private val adminEmailsClient: AdminEmailsClient,
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
-    @Cacheable(cacheNames = ["adminEmails"], unless = "#result.isEmpty()")
     fun getAdminEmails(): List<String> {
-        val response = adminEmailClient.getAdminEmails()
+        val response = adminEmailsClient.getAdminEmails()
         val emails = response.emails
             .map { it.trim() }
             .distinct()
